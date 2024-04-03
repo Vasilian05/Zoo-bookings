2bd27deb5827380caf1fcc9df2c764dfaf386e51
Vasilian05 <alexatanasov559@gmail.com>
2024-04-03 23:39:57 +0100

    tickets
    changes to tickets sessions

--- tickets.php
+++ tickets.php
@@ -1,6 +1,30 @@
 <?php include 'includes/header.php'?>
 
+<?php 
 
+if (isset($_POST['submit'])) {
+    $adult_tickets = $_POST['adult_tickets'];
+    $child_tickets = $_POST['child_tickets'];
+    $baby_tickets = $_POST['baby_tickets'];
+
+    $tickets = array(
+    "adult tickets" => $adult_tickets,
+     "child tickets" => $child_tickets, 
+     "baby_tickets" => $baby_tickets
+    );
+    
+    //try foreach instead of for loop
+    for($i=0; $i < count($tickets); $i++) {
+        
+        if($tickets[$i] > 0) {
+            $_SESSON['Cart'] = $tickets[$i];
+            var_dump( $_SESSION['Cart']);
+        }
+    }
+}
+
+
+?>
 
 
 <h1 class="mt-5 text-center"> Tickets</h1>
@@ -13,11 +37,11 @@
   </div>
   <div class="mb-3">
     <label class="form-label">Child Tickets</label>
-    <input name="child_ticket" type="number" class="form-control">
+    <input name="child_tickets" type="number" class="form-control">
   </div>
   <div class="mb-3">
     <label class="form-label">Baby Tickets</label>
-    <input name="child_ticket" type="number" class="form-control">
+    <input name="baby_tickets" type="number" class="form-control">
   </div>
   <button name="submit"type="submit" class="btn btn-dark">Submit</button>
 </form>
