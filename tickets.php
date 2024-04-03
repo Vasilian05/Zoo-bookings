d7f18bf315a87e2c27122e4308291ca242b13927
Vasilian05 <alexatanasov559@gmail.com>
2024-04-03 17:35:19 +0100

    Create tickets.php

---
+++ tickets.php
@@ -0,0 +1,28 @@
+<?php include 'includes/header.php'?>
+
+
+
+
+<h1 class="mt-5 text-center"> Tickets</h1>
+
+<form  class="w-25 m-auto mt-5"method="POST">
+
+  <div class="mb-3">
+    <label class="form-label">Adult Tickets</label>
+    <input name="adult_tickets" type="number" class="form-control">
+  </div>
+  <div class="mb-3">
+    <label class="form-label">Child Tickets</label>
+    <input name="child_ticket" type="number" class="form-control">
+  </div>
+  <button name="submit"type="submit" class="btn btn-dark">Submit</button>
+</form>
+
+
+
+
+
+
+
+
+<?php include 'includes/footer.php'?>
