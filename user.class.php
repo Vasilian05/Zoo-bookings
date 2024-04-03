cad79ce56882bb1c916a700a8b5773dda9d36dd6
Vasilian05 <alexatanasov559@gmail.com>
2024-04-03 10:46:10 +0100

    Update user.class.php

--- user.class.php
+++ user.class.php
@@ -128,6 +128,21 @@ Class User extends Dbh {
 
     //getting the user from db
 
+    public function getUser($user_id){
+
+        $stmt = $this->connect()->prepare('SELECT * FROM User WHERE user_id = ?');
+
+        if($stmt->execute([$user_id])){
+            
+            //if user is found
+            return $stmt->fetchAll();
+        } else {
+            //user is not found
+            echo 'user not found';
+        }
+
+    }
+
     //loyalty discount
 
     //
