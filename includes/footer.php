c65c0a415d74810c5106e67b17110bd253f7bd7c
Vasilian05 <alexatanasov559@gmail.com>
2024-04-19 09:17:35 +0100

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,4 +2,18 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
+
+
+
+  <div class="container">
+  <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Privacy Policy</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Terms and conditions</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQ</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+    </ul>
+    <p class="text-center text-muted">© 2024 Rigit Zoo Adventure, Inc</p>
+  </footer>
+</div>
 </html>
